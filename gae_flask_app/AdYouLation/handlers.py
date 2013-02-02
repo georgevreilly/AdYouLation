1441fc624ae627e626a5ea66fa7f6f2e3d902d03
George V. Reilly <george@reilly.org>
2013-02-02 15:12:39 -0800

    Each reload consumes 2 choices from session playlist

--- gae_flask_app/AdYouLation/handlers.py
+++ gae_flask_app/AdYouLation/handlers.py
@@ -3,13 +3,19 @@ from flask import render_template, current_app
 from gaesessions import get_current_session
 import random
 
-@AdYouLation.route('/start')
-def start():
-    session = get_current_session()
-    counter = session.get('counter', 0)
-    session['counter'] =  counter + 1 
- 
+def create_playlist():
     videos = current_app.videos
     keys = videos["videos"].keys()
     random.shuffle(keys)
-    return render_template("start.html", names=keys, counter=counter)
+    return keys
+
+@AdYouLation.route('/start')
+def start():
+    session = get_current_session()
+    playlist = session.get('playlist')
+    if not playlist:
+        playlist = create_playlist()
+    choices = playlist[:2]
+    session['playlist'] = remaining = playlist[2:]
+ 
+    return render_template("start.html", choices=choices, remaining=remaining)
