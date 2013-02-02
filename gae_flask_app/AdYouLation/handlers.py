820b9279f3209b1f63e235ddebd8c56fcac248fc
George V. Reilly <george@reilly.org>
2013-02-02 15:03:29 -0800

    Demo use of gaesessions

--- gae_flask_app/AdYouLation/handlers.py
+++ gae_flask_app/AdYouLation/handlers.py
@@ -1,10 +1,15 @@
 from ..AdYouLation import AdYouLation
 from flask import render_template, current_app
+from gaesessions import get_current_session
 import random
 
 @AdYouLation.route('/start')
 def start():
+    session = get_current_session()
+    counter = session.get('counter', 0)
+    session['counter'] =  counter + 1 
+ 
     videos = current_app.videos
     keys = videos["videos"].keys()
     random.shuffle(keys)
-    return render_template("start.html", names=keys)
+    return render_template("start.html", names=keys, counter=counter)
