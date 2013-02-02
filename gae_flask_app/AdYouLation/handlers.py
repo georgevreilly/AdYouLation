434773e79aa58521a1a502978e3b9088f172a9f5
George V. Reilly <george@reilly.org>
2013-02-02 14:26:57 -0800

    Serve "videos" in random order

--- gae_flask_app/AdYouLation/handlers.py
+++ gae_flask_app/AdYouLation/handlers.py
@@ -1,8 +1,10 @@
 from ..AdYouLation import AdYouLation
 from flask import render_template, current_app
+import random
 
 @AdYouLation.route('/start')
 def start():
     videos = current_app.videos
     keys = videos["videos"].keys()
+    random.shuffle(keys)
     return render_template("start.html", names=keys)
