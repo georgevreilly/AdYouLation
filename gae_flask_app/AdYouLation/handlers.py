20063b56dbbc70daf5e332261641ff8e86c58851
George V. Reilly <george@reilly.org>
2013-02-02 19:09:04 -0800

    Make /start do something useful.
    Introduces the concept, initializes the session.

--- gae_flask_app/AdYouLation/handlers.py
+++ gae_flask_app/AdYouLation/handlers.py
@@ -2,8 +2,10 @@ from ..AdYouLation import AdYouLation
 from flask import render_template, current_app, url_for, redirect
 from flask.ext.wtf import Form, TextField, RadioField, Required, Email
 from gaesessions import get_current_session
+
 import random
 import logging
+import uuid
 
 def create_playlist():
     videos = current_app.videos
@@ -20,14 +22,16 @@ def get_choices_remaining(index=2):
 
 def update_playlist():
     (choices, remaining) = get_choices_remaining()
-    session = get_current_session()
-    session['playlist'] = remaining
+    get_current_session()['playlist'] = remaining
     return (choices, remaining)
 
 @AdYouLation.route('/start')
 def start():
-    choices, remaining = update_playlist()
-    return render_template("start.html", choices=choices, remaining=remaining)
+    session = get_current_session()
+    session['playlist'] = create_playlist()
+    if not session.get('id'):
+        session['id'] = uuid.uuid4()
+    return render_template("start.html")
 
 class VideoVote(RadioField):
     CHOICES = (("UP", "Yes!"), ("down", "No!"))
@@ -46,8 +50,9 @@ def vote_form(videos):
 @AdYouLation.route('/vote', methods=('GET', 'POST'))
 def vote():
     choices, remaining = get_choices_remaining()
+    id = get_current_session()['id']
     form = vote_form(choices)
     if form.validate_on_submit():
         update_playlist()
         return redirect(url_for(".vote"))
-    return render_template("vote.html", form=form, remaining=remaining)
+    return render_template("vote.html", form=form, remaining=remaining, id=id)
